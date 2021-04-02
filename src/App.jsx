2af2bacd9a71333725b81f1e4c04420837e558a2
Willian Silva <willian@wiki4fit.com.br>
2021-04-02 03:44:29 -0300

    Initial config

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './styles/global.scss';
+
+export function App() {
+  return (
+    <>
+      <h1>Reactjs starter config</h1>
+      <h2>Build With</h2>
+      <ul>
+        <li>Reactjs</li>
+        <li>Sass</li>
+        <li>Babel</li>
+        <li>Webpack</li>
+      </ul>
+    </>
+  );
+}
