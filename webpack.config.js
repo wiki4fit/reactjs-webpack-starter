83a7c5dfccf47f68c0672f5b5f57548554527a14
Willian Silva <willian@wiki4fit.com.br>
2021-04-03 05:39:41 -0300

    Feat: Typescript Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,13 @@ const devtool = isDevelopment ? 'eval-source-map' : 'source-map';
 module.exports = {
   mode: environmentMode,
   devtool,
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
@@ -32,7 +32,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\. (j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
